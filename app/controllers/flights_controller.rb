0982a40b1ee45f85b0d0f212e2a3e564e5ae1b8d
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-24 08:12:21 -0400

    fix date options

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map{ |a| [ a.code, a.id ] }
-    @date_options = Flight.select('departure_time').distinct.order(:departure_time).map{ |f| [ f.date_formatted, f.departure_time ]}
+    @date_options = Flight.select('departure_time').distinct.order(:departure_time).map{ |f| [ f.date_formatted, f.departure_time.to_date ] }.uniq
 
     @flights = Flight.search(params[:departing_airport_id], params[:arriving_airport_id], params[:num_passengers], params[:travel_date]).order(:departure_time)
   end
