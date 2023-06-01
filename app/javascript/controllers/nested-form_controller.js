37ec327c5485ff28dd74fd7d42d80c75027917e3
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-01 13:46:54 -0400

    add required true back on to name and email inputs, add removal of required true in remove_association function so that form will submit properly

--- app/javascript/controllers/nested-form_controller.js
+++ app/javascript/controllers/nested-form_controller.js
@@ -4,15 +4,19 @@ export default class extends Controller {
   static targets = ["add_item", "template"]
 
   add_association(event) {
-    event.preventDefault()  
-    const content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf())
-    this.add_itemTarget.insertAdjacentHTML('beforebegin', content)
+    event.preventDefault();  
+    const content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf());
+    this.add_itemTarget.insertAdjacentHTML("beforebegin", content);
   }
 
   remove_association(event) {
     event.preventDefault()
-    const item = event.target.closest(".nested-fields")
-    item.querySelector("input[name*='_destroy']").value = 1
-    item.style.display = 'none'
+    const item = event.target.closest(".nested-fields");
+    const required_inputs = item.querySelectorAll("input");
+    required_inputs.forEach(element => {
+      element.required = false;
+    });
+    item.querySelector("input[name*='_destroy']").value = 1;
+    item.style.display = "none";
   }
 }
